33c93b2d90bcfd33440fef41fffe9b104a92d4fc
abdullee <58566210+abdullee@users.noreply.github.com>
2021-04-25 00:31:27 -0700

    uploading

---
+++ pages/contact/contact.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { unstable_concurrentAct } from 'react-dom/test-utils';
+
+
+
+
+const contact=()=> (
+
+
+    <div className='homepage'>
+    
+    
+
+<h1>
+
+dfsdfsdgdsfgdsfgsd
+
+
+</h1>
+
+
+    </div>
+    
+    
+    
+    )
+    
+
+
+
+
+
+ export default contact;
